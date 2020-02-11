b021f82d2c0378569eac4b87f57e34046fdaaf18
kodolok <kodolok@users.noreply.github.com>
2020-02-11 21:50:38 +0100

    elmaradt óra lehetőség

---
+++ src/app/logging-modal/topic-options/topic-options.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TopicOptionsComponent } from './topic-options.component';
+
+describe('TopicOptionsComponent', () => {
+  let component: TopicOptionsComponent;
+  let fixture: ComponentFixture<TopicOptionsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopicOptionsComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TopicOptionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
