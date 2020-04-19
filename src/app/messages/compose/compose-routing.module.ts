9370f9ff6269a5cbef9787ae90a778bc54116a49
kodolok <kodolok@users.noreply.github.com>
2020-04-19 21:59:42 +0200

    message compose candeactivate

--- src/app/messages/compose/compose-routing.module.ts
+++ src/app/messages/compose/compose-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { ComposePage } from "./compose.page";
+import { ModelDirtyGuard } from "src/app/_guards/model-dirty.guard";
 
 const routes: Routes = [
     {
         path: "",
         component: ComposePage,
+        canDeactivate: [ModelDirtyGuard],
     },
 ];
 
